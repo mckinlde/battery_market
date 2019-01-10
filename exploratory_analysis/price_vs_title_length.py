4b3f94d6e0567997c61d704c6230b82641fd9ae6
mckinled <douglas.e.mckinley@vanderbilt.edu>
2019-01-10 15:37:19 -0800

    created exploratory_analysis folder, added price_vs_title_length script

---
+++ exploratory_analysis/price_vs_title_length.py
@@ -0,0 +1,7 @@
+## let's plot the price of a listing against the number of chars in its title.
+
+# 1] sql select *
+
+# 2] for _ in listings, title_length = len(_.title)
+
+# 3] plot title_length vs price
