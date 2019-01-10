02b6cf792471d927585ccfc2def83295de13b2c0
mckinled <douglas.e.mckinley@vanderbilt.edu>
2019-01-10 14:37:52 -0800

    csvwriter works, with wrong data

--- scrape_batteryjunction.py
+++ scrape_batteryjunction.py
@@ -91,7 +91,7 @@ print(test_price)
 ### Write to CSV
 
 import csv
-with open('eggs.csv', 'wb') as csvfile:
+with open('eggs.csv', 'w', newline='') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=' ',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
     spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
